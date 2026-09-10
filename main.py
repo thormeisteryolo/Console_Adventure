22298cbaa63e88eca71754a37008e975eb0aa51e
thormeisteryolo <thorjonsson09@gmail.com>
2026-09-10 16:15:49 +0200

    HUGE changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import monsterstat 
-
+import random
 
 playerData = {
     "name" : None,
@@ -22,18 +22,47 @@ armorInfo = {
     }
 }
 
-
+locations = ("home", "town", "forest", "cave")
+currentLocation = "home"
 armorTypes = ("light", "medium", "heavy")
 weaponTypes = ("mace", "sword", "greatsword")
 
+monsterCount = len(monsterstat.monsters)
+
+
+monsters = []
+for x in monsterstat.monsters:
+      monsters.append(x)
+
+def locationChanger():
+      x = True
+      while x == True:
+            location = input(f"Where does {playerName} want to go: ").lower()
+            if location in locations:
+                  x = False
+                  return location
+            else:
+                  print("Not a valid location")
+
+def startBattle():
+      enemyCount = random.randint(1, monsterCount)
+      enemies = list(())
+      for x in range(1, enemyCount):
+            enemies.append(monsters[random.randint(0, monsterCount - 1)])
+    
+
+
+      #print(enemies)
+      #print(len(enemies))
 
 playerData["name"] = input("What name does it go by: ")
+playerName = playerData["name"]
 y = True
 while y == True:
-    armor = input("What kind of armor does it wear: ")
+    armor = input("What kind of armor does it wear: ").lower()
     if armor in armorTypes:
             playerData["armor"] = armor
-            weapon = input("What is it's prefered weapon: ")
+            weapon = input("What is it's prefered weapon: ").lower()
             if weapon in weaponTypes:
                     playerData["weapon"] = weapon
                     y = False
@@ -42,4 +71,7 @@ while y == True:
     else:
         print("Not a valid armor type")
 
-print(f"{playerData['name']} wakes up in a cottage not to far from the local town")
+print(f"{playerName} wakes up in a cottage not to far from the local town.")
+currentLocation = locationChanger()
+if currentLocation == "forest":
+      startBattle()
--- monsterstat.py
+++ monsterstat.py
@@ -1,11 +1,17 @@
 monsters = {
     "goblin": {
-
+        "maxhealth": 75,
+        "ap": 5,
+        "damage": 20
     },
     "golem": {
-
+        "maxhealth": 200,
+        "ap": 50,
+        "damage": 30
     },
     "bandit": {
-        
+        "maxhealth": 100,
+        "ap": 10,
+        "damage": 15 
     }
 }
